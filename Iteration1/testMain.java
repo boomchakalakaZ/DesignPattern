3d7cb3887d9276ae42280e71358b3fb374f9e70f
boomchakalakaZ <linhnvu7@gmail.com>
2018-11-11 20:39:00 -0600

    Add files via upload

--- Iteration1/testMain.java
+++ Iteration1/testMain.java
@@ -3,16 +3,18 @@ package Iteration1;
 public class testMain {
 	
 	public static void main(String[] args) {
-		DBMgr dbmgr = new DBMgr();
-		String dbmgrTest;
-		dbmgrTest = dbmgr.saveEvent();
-		System.out.println(dbmgrTest);
-		dbmgrTest = dbmgr.saveRegistration();
-		System.out.println(dbmgrTest);
-		dbmgrTest = dbmgr.updateProposal();
-		System.out.println(dbmgrTest);
-		dbmgrTest = dbmgr.requestEvent();
-		System.out.println(dbmgrTest);
+		DBMgr DBMGR = new DBMgr();
+		Registration REG = new Registration(0.00f);
+		
+		System.out.println(DBMGR.saveEvent());
+		System.out.println(DBMGR.saveRegistration());
+		System.out.println(DBMGR.updateProposal());
+		System.out.println(DBMGR.requestEvent());
+		
+		float fee = REG.getFee();
+		System.out.println(fee);
+		
+		
 	}
 
 }
